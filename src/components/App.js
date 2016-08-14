db967c365a23104318ece35a2e165aa872180d56
Eduardo Pedroso <drdpedroso@gmail.com>
2016-08-14 20:33:08 -0300

    Creating placar behavior and data

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,28 @@ import React from 'react';
 
 import PlacarContainer from './PlacarContainer';
 
+
+const dados = {
+	partida: {
+		estadio: "Maracana/RJ",
+		data: "01/06/2016",
+		horario: "19h",
+	},
+	casa: {
+		nome:"Vasco",
+	},
+	visitante: {
+		nome:"Flamengo",
+	}
+};
+
 export default class App extends React.Component {
 	render(){
 		return (
 			<div>
-				<PlacarContainer />
+				<PlacarContainer partida={dados.partida}
+								 casa={dados.casa}
+							 	 visitante={dados.visitante} />
 			</div>				
 		);
 	}
