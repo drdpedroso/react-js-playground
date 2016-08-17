522a5e4ea6af9220bbfe028bab91797e82191320
Eduardo Pedroso <drdpedroso@gmail.com>
2016-08-17 18:55:07 -0300

    Adding IndexRoute amd creating home screen

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { Router, Route, hashHistory} from 'react-router'
+import { Router, Route, IndexRoute, hashHistory} from 'react-router'
 
 import App from "./pages/App";
 import Cursos from "./pages/Cursos";
 import Sobre from "./pages/Sobre";
+import Home from "./pages/Home";
 
 ReactDom.render((
 		<Router history={hashHistory}>
 			<Route path="/" component={App}>
+				<IndexRoute component={Home} />
 				<Route path="/cursos" component={Cursos} />
 				<Route path="/sobre" component={Sobre} />
 			</Route>
