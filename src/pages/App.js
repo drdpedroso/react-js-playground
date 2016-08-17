45c43a3b4f4ae8514f15fd542217a41c24512d5d
Eduardo Pedroso <drdpedroso@gmail.com>
2016-08-17 18:48:56 -0300

    Adding Link component from react router to navbar

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router'
+
 
 export default class App extends React.Component {
 	render() {
@@ -6,9 +8,12 @@ export default class App extends React.Component {
 			<div>
 				<nav className="nav is-tab has-shadow">
 					<div className="nav-left">
-						<a href="#" className="nav-item">Home</a>
-						<a href="#" className="nav-item">Cursos</a>
-						<a href="#" className="nav-item">Sobre</a>
+						<Link to="/" className="nav-item is-tab"
+							  activeClassName="is-active">Home</Link>
+						<Link to="/cursos" className="nav-item is-tab"
+					 		  activeClassName="is-active">Cursos</Link>
+						<Link to="/sobre" className="nav-item is-tab"
+							  activeClassName="is-active">Sobre</Link>
 					</div>
 				</nav>
 
